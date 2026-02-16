67d224db4d1d078d5d70dc378b95a91bad22ffcf
K-Ayaan <kunwarayaan9655@gmail.com>
2026-02-16 10:46:23 +0530

    Your commit message here

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,60 @@
+
+/**
+ *
+ * MAIN CLASS - PalindromeCheckerApp
+ *
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Palindrome Checker Management System.
+ *
+ * At this stage, the application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message
+ * - Shows application version
+ * - Accepts user input
+ * - Checks whether the input is a palindrome
+ *
+ * @author KokoMelon
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    /**
+     * Application entry point.
+     *
+     * This is the first method executed by the JVM
+     * when the program starts.
+     *
+     * @param args Command-line arguments
+     */
+
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System Initialized Successfully");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

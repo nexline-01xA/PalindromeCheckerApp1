f0eab25187c3cd08785e16a1b6a23952cd681a1e
nexline-01xA <hn9608@srmist.edu.in>
2026-02-27 15:53:40 +0530

    UC2 Palindrome hardcoded validation implemented

---
+++ App/src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,27 @@
+
+public class UseCase2PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC2.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length.
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("\"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a Palindrome.");
+        }
+    }
+}
